710e66320d160157ea77c9956c153c63feb016db
minju kim <demin1026@kpu.ac.kr>
2023-08-19 05:13:08 +0900

    불필요한 file delete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 
@@ -6,28 +5,6 @@ function App() {
   return (
     <div className='App'>
       <DiaryEditor />
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> a100d4b6e39679442ee073b2b18684aecdaf66d4
     </div>
   );
 }
